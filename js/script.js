79243fcf3f956a242dbdfbb6685647e2b87283f3
Van Nguyen <99274250+van-nguyen2006@users.noreply.github.com>
2022-04-07 08:24:40 +0000

    Added more comments.

--- js/script.js
+++ js/script.js
@@ -5,14 +5,15 @@
  * This function calculates the area of a trapezoid.
  */
 function calculate () {
-  // input makes value a float
-  let height = parseFloat(document.getElementById('height').value)
-  let baseA = parseFloat(document.getElementById('base-a').value)
-  let baseB = parseFloat(document.getElementById('base-b').value)
+  // Variables
+  // Value is made into Float
+  let height = parseFloat(document.getElementById('height').value) // Stores value from user
+  let baseA = parseFloat(document.getElementById('base-a').value) // Stores value from user
+  let baseB = parseFloat(document.getElementById('base-b').value) // Stores value from user
 
-  // process (formula for area)
+  // Process (calculates area)
   let area = (baseA + baseB) / 2 * height
 
-  // output
+  // Outputs Area
   document.getElementById('area').innerHTML = 'Area is: ' + area.toFixed(2) + ' cm²'
 }
